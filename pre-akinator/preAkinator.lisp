f72c5860352d853cff98ac79b4cb325d1665fddb
osvi-dev <joseconstantino987@gmail.com>
2024-10-08 22:17:57 -0600

    Estructura inicial del arbol

---
+++ pre-akinator/preAkinator.lisp
@@ -0,0 +1,26 @@
+(defparameter *nodes* '((Gryffindor (
+										(Harry Potter (Bueno))
+										(Hermione Granger (Bueno))
+										(Ron Weasley (Bueno))
+										(Albus Dumbledore (Bueno)) 
+										(Sirius Black (Bueno))))
+						(Slytherin (
+										(Draco Malfoy (Malo))
+										(Severus Snape (Malo))
+										(Lord Voldemort (Malo))
+										(Bellatrix Lestrange (Malo))
+										(Lucius Malfoy (Malo))))
+
+						(Hufflepuff (
+										(Cedric Diggory (Bueno))
+										(Nymphadora Tonks (Bueno))
+										(Pomona Sprout (Bueno)))) 
+
+						(Ravenclaw   (
+										(Luna Lovegood (Buena))
+										(Cho Chang (Buena))
+										(Filius Flitwick (Buena))))
+
+
+
+))
