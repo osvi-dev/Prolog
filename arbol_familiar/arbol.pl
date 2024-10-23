ee85e2a3fd46a3b9249dafa7ad190de9c77c4539
osvi-dev <joseconstantino987@gmail.com>
2024-10-23 00:20:01 -0600

    avance de arbol familiar

---
+++ arbol_familiar/arbol.pl
@@ -0,0 +1,152 @@
+hijo(jose_antonio).
+hijo(osvaldo).
+
+hijo(salvador).
+hijo(tony).
+hijo(chavita).
+
+hijo(erick).
+
+hijo(isidro).
+hijo(jair).
+hijo(daniel).
+
+hijo(juan).
+hijo(juanito).
+
+
+hijo(jose).
+
+hijo(felix).
+
+hijo(manuel).
+hijo(lalo).
+hijo(eitan).
+
+hijo(josep).
+
+hijo(cacheton).
+
+hijo(emerick).
+hijo(emilio).
+
+hijo(pato).
+
+hija(patricia).
+hija(fatima).
+
+hija(gabriela).
+hija(gaby_chiquita).
+hija(camila).
+hija(mary).
+hija(regina).
+hija(yaya).
+hija(natalia).
+
+hija(ana).
+hija(lucero).
+
+hija(cristina).
+hija(jennifer).
+hija(evelyn).
+
+hija(anita).
+hija(michelle).
+hija(alexa).
+
+hija(china).
+hija(isabella).
+
+hija(estefani).
+hija(soff).
+
+hija(hermila).
+hija(miriam).
+hija(ailin).
+hija(yareli).
+hija(jazmin).
+hija(sarahi).
+
+hija(xochilt_hija).
+
+padre(jose).
+padre(jose_antonio).
+padre(salvador).
+
+padre(isidro_abuelo).
+padre(isidro).
+padre(juan).
+
+padre(jaime).
+padre(felix).
+padre(vidal).
+padre(nacho).
+padre(miguel).
+padre(edgar).
+padre(angel).
+padre(hugo).
+padre(luis).
+
+madre(teresa).
+
+madre(rafaela).
+madre(patricia).
+
+madre(gabriela).
+madre(yaya).
+madre(gaby_chiquita).
+madre(mary).
+
+madre(ana).
+madre(anita).
+madre(lucero).
+madre(cristina).
+
+
+madre(erika).
+madre(china).
+
+madre(estefani).
+
+madre(hermila).
+madre(miriam).
+
+madre(xochilt).
+
+padrede(jose, jose_antonio).
+padrede(jose, salvador).
+padrede(jose, hermila).
+
+padrede(isidro_abuelo, patricia).
+padrede(isidro_abuelo, isidro).
+padrede(isidro_abuelo, erick).
+padrede(isidro_abuelo, juan).
+padrede(isidro_abuelo, gabriela).
+padrede(isidro_abuelo, ana).
+
+padrede(jose_antonio, osvaldo).
+padrede(jose_antonio, fatima).
+
+padrede(salvador, tony).
+padrede(salvador, chavita).
+padrede(salvador, xochilt_hija).
+
+padrede(isidro, daniel).
+padrede(isidro, jair).
+padrede(isidro, china).
+
+padrede(juan, juanito).
+
+padrede(jaime, jose).
+
+padrede().
+
+
+
+
+
+
+
+
+
+
